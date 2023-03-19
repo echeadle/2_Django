1772bff6aa6f4f330a89eb3c1962efffdf263146
echeadle <echeadle@gmail.com>
2023-03-19 12:00:27 -0600

    Add filter posts by tags

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -5,8 +5,10 @@ app_name = 'blog'
 
 urlpatterns = [
     # Post views
-    # path('', views.post_list, name='post_list'),
-    path('', views.PostListView.as_view(), name='post_list'),
+    path('', views.post_list, name='post_list'),
+    # path('', views.PostListView.as_view(), name='post_list'),
+    path('tag/<slug:tag_slug>/',
+         views.post_list, name='post_list_by_tag'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          views.post_detail,
          name='post_detail'),
@@ -14,4 +16,5 @@ urlpatterns = [
          views.post_share, name='post_share'),
     path('<int:post_id>/comment/',
          views.post_comment, name='post_comment'),
+
 ]
