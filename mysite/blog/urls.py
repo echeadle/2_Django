75cc9ba980e9b7775e3b016008d0428a6e9f2f78
echeadle <echeadle@gmail.com>
2023-03-19 10:28:57 -0600

    Add comments forms and handling

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    # post views
+    # Post views
     # path('', views.post_list, name='post_list'),
     path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
@@ -12,4 +12,6 @@ urlpatterns = [
          name='post_detail'),
     path('<int:post_id>/share/',
          views.post_share, name='post_share'),
+    path('<int:post_id>/comment/',
+         views.post_comment, name='post_comment'),
 ]
